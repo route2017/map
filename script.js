ae54176ee9ab6715957417d8e04610dd3f8c37f0
route2017 <route2017@users.noreply.github.com>
2016-12-22 22:31:26 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+
+ymaps.ready(init);
+
+var myMap;
+
+function init(){     
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10
+    });
+}
+
+    
+
+function handleFileSelect(evt) {
+    var files = evt.target.files;
+    
+    if ( !(/\.csv$/.test(files[0].name)) ) {
+        document.getElementById('message').innerText = "File should be CSV!";
+        return;
+    }
+    
+    var reader = new FileReader();
+    
+    reader.onload = (function(theFile) {
+        
+        return function(e) {
+            var res = e.target.result.split("\n");
+            setNewPoints(res);
+        }
+        
+    })(files[0]);
+
+    reader.readAsText(files[0], 'CP1251');
+    
+}
+
+document.getElementById('upload-input').addEventListener('change', handleFileSelect, false);
+
+function setNewPoints(data) {
+    for (var x = 0; x < data.length; x++) {
+        
+        if (data[x]) {
+            var line = data[x].split(";");
+            var pointName = line[0];
+            var pointCoord = line[1].split(",");
+        
+            var newPoint = new ymaps.Placemark(pointCoord, { 
+                balloonContent: pointName 
+            });
+    
+            myMap.geoObjects.add(newPoint);   
+        }
+        
+    }
+}
